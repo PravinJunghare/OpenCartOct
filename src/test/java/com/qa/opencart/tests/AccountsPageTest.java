f669d5536459b5c2dd353f651a82b54c5d6e502c
PravinJunghare <PravinJunghare01@gmail.com>
2024-10-23 10:39:52 +0530

    Day2 Added ElementUtil and  Constants

--- src/test/java/com/qa/opencart/tests/AccountsPageTest.java
+++ src/test/java/com/qa/opencart/tests/AccountsPageTest.java
@@ -1,5 +1,59 @@
 package com.qa.opencart.tests;
 
-public class AccountsPageTest {
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.qa.opecart.base.BaseTest;
+import com.qa.opencart.constants.AppConstant;
+
+public class AccountsPageTest extends BaseTest {
+	// precodition is that user should be login for AccountsPage
+
+	@BeforeClass
+
+	public void accPageSetup() {
+		accountsPage = loginPage.doLogin("hacaxa3208@craftapk.com", "Test@1234");
+		//accountsPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+
+	}
+
+	@Test
+	public void accountsPageTitleTest() {
+		String actTitle = accountsPage.getAccPageTitle();
+		// Assert.assertEquals(actTitle, "My Account");
+		Assert.assertEquals(actTitle, AppConstant.ACCOUNTS_PAGE_TITLE_VALUE);
+	}
+
+	@Test
+	public void accountsPageUrlTest() {
+		String actUrl = accountsPage.getAccPageUrl();
+		// Assert.assertTrue(actUrl.contains(""));
+		Assert.assertTrue(actUrl.contains(AppConstant.ACCOUNTS_PAGE_URL_FRACTION_VALUE));
+	}
+
+	@Test
+	public void isLogoutLinkExistTest() {
+		Assert.assertTrue(accountsPage.isLogoutLinkExist());
+	}
+
+	@Test
+	public void accountHeaderTest() {
+		List<String> actualAccHeaderList = accountsPage.getAccountPageHeadersList();
+		System.out.println(actualAccHeaderList);
+		// Assert.assertEquals(actualAccHeaderList.size(), 4);
+		Assert.assertEquals(actualAccHeaderList.size(), AppConstant.ACCOUNTS_PAGE_HEADERCOUNT);
+	}
+
+	@Test
+	public void accountHeaderValueTest() {
+		List<String> actualAccHeaderList = accountsPage.getAccountPageHeadersList();
+		System.out.println("Actual AccPage header list" + actualAccHeaderList);
+		System.out.println("Expected AccPage header list " + AppConstant.EXPECTED_ACCOUNTPAGE_HEADERS_LIST);
+		Assert.assertEquals(actualAccHeaderList, AppConstant.EXPECTED_ACCOUNTPAGE_HEADERS_LIST);
+
+	}
 
 }
--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -4,21 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.qa.opecart.base.BaseTest;
+import com.qa.opencart.constants.AppConstant;
 
 public class LoginPageTest extends BaseTest {
 
 	@Test(priority = 1)
 	public void loginPageTitleTest() {
 		String actTitle = loginPage.getLoginPageTitle();
-		Assert.assertEquals(actTitle, "Account Login");
+		// Assert.assertEquals(actTitle, "Account Login");
+		Assert.assertEquals(actTitle, AppConstant.LOGIN_PAGE_TITLE_VALUE);
 
 	}
 
 	@Test(priority = 2)
 	public void loginPageUrlTest() {
 		String actUrl = loginPage.getLoginUrl();
-		Assert.assertTrue(actUrl.contains("route=account/login"));
-
+		// Assert.assertTrue(actUrl.contains("route=account/login"));
+		Assert.assertTrue(actUrl.contains(AppConstant.LOGIN_PAGE_URL_FRACTION_VALUE));
 	}
 
 	@Test(priority = 3)
@@ -28,7 +30,11 @@ public class LoginPageTest extends BaseTest {
 
 	@Test(priority = 4)
 	public void loginTest() {
-		loginPage.doLogin("hixiken531@leacore.com", "Test@1234");
-
+		// loginPage.doLogin("pravinjunghare01@yahoo.com", "Test@1234");
+		accountsPage = loginPage.doLogin("hacaxa3208@craftapk.com", "Test@1234");
+		// accountsPage = loginPage.doLogin(prop.getProperty("username").trim(),
+		// prop.getProperty("password").trim());
+		Assert.assertTrue(accountsPage.isLogoutLinkExist());
 	}
+
 }
